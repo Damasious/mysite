947f924129925ceb2cec7115e20492d685561835
Damasious <fernando.damasious77@gmail.com>
2024-05-18 18:41:23 -0300

    Configure pytest

---
+++ mysite/blog/factories.py
@@ -0,0 +1,37 @@
+from typing import Any, MutableMapping
+import factory
+from faker import Factory as FakerFactory
+
+from django.contrib.auth.models import User
+from django.utils.timezone import now
+
+from blog.models import Post
+
+faker = FakerFactory.create()
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+        
+    email = factory.Faker("safe_email")
+    username = factory.LazyAttribute(lambda x: faker.name())
+    
+    @classmethod
+    def __prepare(cls, create, **kwargs):
+        password = kwargs.pop("password", None)
+        user = super(UserFactory, cls).prepare(create, **kwargs)
+        if password:
+            user.set_password(password)
+            if create:
+                user.save()
+        return user
+    
+
+class PostFactory(factory.django.DjangoModelFactory):
+    title = factory.LazyAttribute(lambda x: faker.sentence())
+    created_on = factory.LazyAttribute(lambda x: now())
+    author = factory.SubFactory(UserFactory)
+    status = 0
+    
+    class Meta:
+        model = Post
